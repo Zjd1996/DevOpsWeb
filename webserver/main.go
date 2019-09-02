e09e8b4c578c505beb57c558051f7623085b8ff9
赵江东 <zhaojiangdong@MacBook-Pro-X.local>
2019-09-02 22:11:13 +0800

    try 3

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1> Hello, this is my event page 1005!</h1>")
+	io.WriteString(w, "<h1> Hello, this is my event page 1010!</h1>")
 }
 
 func main() {
