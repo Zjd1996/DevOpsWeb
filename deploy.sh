1036ad0e7af06ef705f54f12d300220f7070c762
赵江东 <zhaojiangdong@MacBook-Pro-X.local>
2019-09-02 22:02:37 +0800

    make it more and more productive...

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 kill -9 $(pgrep webserver)
-git pull https://github.com/Zjd1996/DevOpsWeb.git
-cd DevOpsWeb/webserver/
+cd ~/DevOpsWeb
+git pull 
+cd ~/DevOpsWeb/webserver/
 ./webserver &
